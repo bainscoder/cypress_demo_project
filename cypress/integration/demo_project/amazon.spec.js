c8480365a8f804a0960e6e7ddd4a01f439c455ac
Manpreet Bains <manpreetbains@clanap.com>
2021-04-28 23:31:04 +0530

    add two teset script with POM structure

--- cypress/integration/demo_project/amazon.spec.js
+++ cypress/integration/demo_project/amazon.spec.js
@@ -5,39 +5,51 @@ import amazonhomepage       from '../../fixtures/amazonhomepage.json'
 import resultpage           from '../../fixtures/resultpage.json'
 import productdetailpage    from '../../fixtures/productdetailpage.json'
 import amazonsigninpage     from '../../fixtures/amazonsigninpage.json'
+import amazonsignuppage     from '../../fixtures/amazonsignuppage.json'
 
 // for importing Class files
 import AmazonHomePage       from '../../support/pageObject/amazonHomePage'
 import ResultPage           from '../../support/pageObject/resultPage'
 import ProductDetailPage    from '../../support/pageObject/productDetailPage'
 import AmazonSignInPage     from '../../support/pageObject/amazonSignInPage'
+import AmazonSignupPage     from '../../support/pageObject/amazonSignUpPage'
 
 const amazonHomePage    = new AmazonHomePage()
 const resultPage        = new ResultPage()
 const productDetailPage = new ProductDetailPage()
-const amazonSignInPage  = new AmazonSignInPage
+const amazonSignInPage  = new AmazonSignInPage()
+const amazonSignUpPage  = new AmazonSignupPage()
 
 describe('Cypress automation framework for demo purpose', ()=>{
 
-    it('Amazon: First test', ()=>{
-          
-        cy.visit('https://www.amazon.in')
+    beforeEach('', ()=>{
+
+        cy.visit('/')                                                                                                                       // to visit Amazon India website
 
     })
-    it.only('Amazon: Second test', ()=>{
 
-        cy.visit('/')                                                                                                                       // visit Amazon India site
+    it('Amazon_First_script: Guest user should able to search, find and add product into the cart', ()=>{
+
         amazonHomePage.getTitleBartext().should('contain', amazonhomepage.addressText)                                                      // to verify text in Amazon home page
         amazonHomePage.getSearchfield().type(amazonhomepage.productname)                                                                    // add item name in the search field
-        resultPage.getProductitem().eq(2).should('contain', resultpage.productTitle).invoke('removeAttr', 'target').click().wait(2000)      // click searched item
-        productDetailPage.getAddtoCartbutton().should('contain', productdetailpage.addToCarttext)                                           // to verify and click "Add to Cart" button
-        cy.get('#addToCart_feature_div .a-button-inner  #add-to-cart-button').click()
-        amazonSignInPage.getSignIntext().should('contain', amazonsigninpage.signIntext)                                                       // to verify main title of the page
+        resultPage.getProductitem().should('contain', resultpage.productTitle).invoke('removeAttr', 'target').click().wait(2000)            // click searched item
+        productDetailPage.getAddtoCartbutton().should('contain', productdetailpage.addToCarttext).click()                                   // to verify and click "Add to Cart" button        
+        amazonSignInPage.getSignIntext().should('contain', amazonsigninpage.signIntext)                                                     // to verify main title of the page
         amazonSignInPage.getCreateNewAccountbutton().should('contain', amazonsigninpage.createAccounttext)                                  // to verify Create your Amazon account
 
     })
+    
+    it('Amazon_Second_script: Guest user should able add invalid-data in Sign-Up page and get valid error message', ()=>{
         
-
-
+        amazonHomePage.getAccountbutton().should('contain', 'Account & Lists').click()                                                      // to verify and click Account and Lists button
+        amazonSignInPage.getCreateNewAccountbutton().should('contain', amazonsigninpage.createAccounttext).click()                          // to verify Create your Amazon account
+        amazonSignUpPage.getPagetitle().should('contain', amazonsignuppage.pageTitle)                                                       // to verify Create Account title
+        amazonSignUpPage.getYourNamefield().type(amazonsignuppage.name)                                                                     // to add data in Your name field
+        amazonSignUpPage.getMobileNumberfield().type(amazonsignuppage.number)                                                               // to add data in mobile number field
+        amazonSignUpPage.getPasswordfield().type(amazonsignuppage.password)                                                                 // to add data in password field
+        amazonSignUpPage.getContinuebutton().should('contain', amazonsignuppage.continueButton).click()                                     // to verify and click Continue button
+        amazonSignUpPage.getErrormessage().should('contain', amazonsignuppage.errorMessage)                                                 // to verify error message
+        
+    })
 
 })
